f3ce44d1d934b597ae768553a8b317beb64f3621
Pavithra Thangaraj <pavithrathangaraj555@gmail.com>
2025-09-22 12:14:04 +0530

    remove un used imports

--- src/App.js
+++ src/App.js
@@ -2,13 +2,8 @@ import "./App.css";
 import {  Container,  } from "@mui/material";
 import Header from "./components/Header";
 import Pages from "./components/Pages";
-import Formsbs from "./components/Forms_bs";
-import Formsmui from "./components/Forms_mui";
 import Hero from "./components/Hero";
-import Card from './components/Cards';
 import Middle from "./components/Middle";
-import ReadMe from "./components/ReadMe";
-import { Route, Routes } from "react-router-dom";
 function App() {
   return (
     <div className="" >
